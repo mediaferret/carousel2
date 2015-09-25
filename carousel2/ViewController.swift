c16aade12ca29344e3281e2964b1174906a92741
mediaferret <mediaferret@gmail.com>
2015-09-24 19:08:07 -0700

    Initial Commit

---
+++ carousel2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  carousel2
+//
+//  Created by Kevin O'Connell on 9/24/15.
+//  Copyright © 2015 Kevin O'Connell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
